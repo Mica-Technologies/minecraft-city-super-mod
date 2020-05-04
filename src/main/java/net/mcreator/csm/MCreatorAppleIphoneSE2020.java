8f23aaf5d8d4aa679a56046849d4fe4e93f91917
MICA_ADMIN <webmaster@micatechnologies.com>
2020-05-04 15:15:10 -0700

    Added 2020 iPhone SE

---
+++ src/main/java/net/mcreator/csm/MCreatorAppleIphoneSE2020.java
@@ -0,0 +1,68 @@
+package net.mcreator.csm;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import java.util.List;
+
+@Elementscsm.ModElement.Tag
+public class MCreatorAppleIphoneSE2020 extends Elementscsm.ModElement {
+	@GameRegistry.ObjectHolder("csm:appleiphonese2020")
+	public static final Item block = null;
+
+	public MCreatorAppleIphoneSE2020(Elementscsm instance) {
+		super(instance, 1123);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("csm:appleiphonese2020", "inventory"));
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("appleiphonese2020");
+			setRegistryName("appleiphonese2020");
+			setCreativeTab(MCreatorMCLATechTab.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<String> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add("This iPhone does nothing and is only for looks!");
+		}
+	}
+}
