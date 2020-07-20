9e675f99d0b694b6983d76121ab664c6409550d0
MICA_ADMIN <webmaster@micatechnologies.com>
2020-07-19 19:26:21 -0700

    Added new EAS sounds for future weather radios

--- src/main/java/net/mcreator/csm/ElementsCitySuperMod.java
+++ src/main/java/net/mcreator/csm/ElementsCitySuperMod.java
@@ -169,6 +169,14 @@ public class ElementsCitySuperMod implements IFuelHandler, IWorldGenerator {
 		sounds.put(new ResourceLocation("csm", "oldradio2"), new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "oldradio2")));
 		sounds.put(new ResourceLocation("csm", "threestooges_ve"), new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "threestooges_ve")));
 		sounds.put(new ResourceLocation("csm", "marchtime_as"), new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "marchtime_as")));
+		sounds.put(new ResourceLocation("csm", "alto_tornado_warning_newpaul"),
+				new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "alto_tornado_warning_newpaul")));
+		sounds.put(new ResourceLocation("csm", "alto_tornado_warning_tom"),
+				new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "alto_tornado_warning_tom")));
+		sounds.put(new ResourceLocation("csm", "alto_weather_forecast_newpaul"),
+				new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "alto_weather_forecast_newpaul")));
+		sounds.put(new ResourceLocation("csm", "alto_weather_forecast_tom"),
+				new net.minecraft.util.SoundEvent(new ResourceLocation("csm", "alto_weather_forecast_tom")));
 	}
 
 	public void preInit(FMLPreInitializationEvent event) {
