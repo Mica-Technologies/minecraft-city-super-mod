24c7f33a2746d9e19d32292c42e91b9ce64a8ead
MICA_ADMIN <webmaster@micatechnologies.com>
2020-08-14 09:44:13 -0400

    Begin constants implementation

--- src/main/java/com/micatechnologies/minecraft/csm/sys/ModConstants.java
+++ src/main/java/com/micatechnologies/minecraft/csm/sys/ModConstants.java
@@ -1,5 +1,27 @@
 package com.micatechnologies.minecraft.csm.sys;
 
-public class ModConstants {
+/**
+ * Class containing constant values and information for the mod.
+ *
+ * @author ah@micatechnologies.com
+ * @version N/T
+ * @since 2020.6
+ */
+public class ModConstants
+{
+    /**
+     * The namespace of the mod, which is used to identify the mod and its elements. The namespace prefixes each block
+     * name; for example, in the block ID `csm:testblock1`, the mod namespace is `csm` and the block name is
+     * `testblock1`.
+     * <p>
+     * Note: Changing this value has little effect on new maps, but existing maps will lost any previously placed blocks
+     * in the previous namespace.
+     */
     public static final String MOD_NAMESPACE = "csm";
+
+    /**
+     * The version of the mod. This number should be incremented by 0.0.1 for bug fix releases, incremented by 0.1 for
+     * feature and bug fix releases, or incremented by 1.0 for large or breaking changes.
+     */
+    public static final String MOD_VERSION = "2020.6-dev";
 }
