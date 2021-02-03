799cffcda972b1eb5342ca0327ac4dfbd0cec9bf
MICA_ADMIN <webmaster@micatechnologies.com>
2021-02-03 10:46:35 -0500

    Start AbstractBrightLight impl

--- src/main/java/com/micatechnologies/minecraft/csm/block/BlockAEATB0.java
+++ src/main/java/com/micatechnologies/minecraft/csm/block/BlockAEATB0.java
@@ -1,6 +1,7 @@
 package com.micatechnologies.minecraft.csm.block;
 
 import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import com.micatechnologies.minecraft.csm.lighting.AbstractBrightLight;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -30,7 +31,9 @@ import com.micatechnologies.minecraft.csm.creativetab.TabMCLAElectricTab;
 
 @ElementsCitySuperMod.ModElement.Tag
 public class BlockAEATB0 extends ElementsCitySuperMod.ModElement {
-	@GameRegistry.ObjectHolder("csm:aeatb0")
+	private static final String elementId = "aeatb0";
+
+	@GameRegistry.ObjectHolder("csm:"+elementId)
 	public static final Block block = null;
 	public BlockAEATB0(ElementsCitySuperMod instance) {
 		super(instance, 349);
@@ -45,29 +48,11 @@ public class BlockAEATB0 extends ElementsCitySuperMod.ModElement {
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("csm:aeatb0", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("csm:"+elementId
+				, "inventory"));
 	}
-	public static class BlockCustom extends Block {
-		public static final PropertyDirection FACING = BlockDirectional.FACING;
-		public BlockCustom() {
-			super(Material.ROCK);
-			setRegistryName("aeatb0");
-			setUnlocalizedName("aeatb0");
-			setSoundType(SoundType.STONE);
-			setHarvestLevel("pickaxe", 1);
-			setHardness(2F);
-			setResistance(10F);
-			setLightLevel(0F);
-			setLightOpacity(0);
-			setCreativeTab(TabMCLAElectricTab.tab);
-			this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-		}
-
-		@Override
-		public boolean isFullCube(IBlockState state) {
-			return false;
-		}
-
+	public static class BlockCustom extends AbstractBrightLight
+	{
 		@Override
 		public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
 			switch ((EnumFacing) state.getValue(BlockDirectional.FACING)) {
@@ -86,76 +71,10 @@ public class BlockAEATB0 extends ElementsCitySuperMod.ModElement {
 					return new AxisAlignedBB(0D, 0D, 1D, 1D, 1D, 0.5D);
 			}
 		}
-		public static final PropertyBool POWERED = PropertyBool.create("powered");
-		@Override
-		public void neighborChanged(IBlockState state, World world, BlockPos pos, Block blockIn, BlockPos p_189540_5_) {
-			int powered = world.isBlockIndirectlyGettingPowered(pos);
-			world.setBlockState(pos, state.withProperty(POWERED, powered > 0), 3);
-			// Powered, Add Light
-			if (powered > 0) {
-				BlockPos doAddAt = null;
-				for (int findy = -1; findy >= -40; findy--) {
-					BlockPos test = new BlockPos(pos.getX(), pos.getY() + findy, pos.getZ());
-					IBlockState bs = world.getBlockState(test);
-					Block block = bs.getBlock();
-					// found block that is not air
-					if (block.isAir(bs, world, test) || block.isReplaceable(world, test)) {
-						// dont add light if block is right below street light
-						if (findy < -1)
-							doAddAt = test;
-					} else
-						break;
-				}
-				// add if marked for add
-				if (doAddAt != null) {
-					world.setBlockState(doAddAt, Block.getBlockFromName("csm:lightupair").getDefaultState(), 3);
-				}
-			}
-			// Not powered, remove light
-			else {
-				for (int findy = -1; findy >= -40; findy--) {
-					BlockPos test = new BlockPos(pos.getX(), pos.getY() + findy, pos.getZ());
-					IBlockState bs = world.getBlockState(test);
-					// stop removing light once hit block
-					if (bs.getBlock() == Block.getBlockFromName("csm:lightupair"))
-						world.setBlockToAir(test);
-				}
-			}
-		}
 
 		@Override
-		public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-			world.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
-		}
-
-		public static EnumFacing getFacingFromEntity(BlockPos clickedBlock, EntityLivingBase entity) {
-			return EnumFacing.getFacingFromVector((float) (entity.posX - clickedBlock.getX()), (float) (entity.posY - clickedBlock.getY()),
-					(float) (entity.posZ - clickedBlock.getZ()));
-		}
-
-		@Override
-		public IBlockState getStateFromMeta(int meta) {
-			return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7)).withProperty(POWERED, (meta & 8) != 0);
-		}
-
-		@Override
-		public int getMetaFromState(IBlockState state) {
-			return state.getValue(FACING).getIndex() + (state.getValue(POWERED) ? 8 : 0);
-		}
-
-		@Override
-		protected BlockStateContainer createBlockState() {
-			return new BlockStateContainer(this, FACING, POWERED);
-		}
-
-		@Override
-		public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
-			return state.getValue(POWERED) == true ? 15 : 0;
-		}
-
-		@Override
-		public boolean isOpaqueCube(IBlockState state) {
-			return false;
+		public String getBlockRegistryName() {
+			return elementId;
 		}
 	}
 }
--- src/main/java/com/micatechnologies/minecraft/csm/lighting/AbstractBrightLight.java
+++ src/main/java/com/micatechnologies/minecraft/csm/lighting/AbstractBrightLight.java
@@ -5,28 +5,31 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.util.Random;
+import javax.annotation.Nullable;
 
 public abstract class AbstractBrightLight extends Block
 {
 
-    public static final PropertyDirection FACING = BlockDirectional.FACING;
+    public static final PropertyDirection FACING  = BlockDirectional.FACING;
+    public static final PropertyBool      POWERED = PropertyBool.create( "powered" );
 
     public AbstractBrightLight() {
         super( Material.ROCK );
+        setRegistryName( getBlockRegistryName() );
         setUnlocalizedName( getBlockRegistryName() );
         setSoundType( SoundType.GROUND );
         setHarvestLevel( "pickaxe", 1 );
@@ -45,31 +48,12 @@ public abstract class AbstractBrightLight extends Block
     }
 
     @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer( this, FACING );
-    }
-
-    @Override
-    public IBlockState getStateFromMeta( int meta ) {
-        return this.getDefaultState().withProperty( FACING, EnumFacing.getFront( meta ) );
-    }
-
-    @Override
-    public int getMetaFromState( IBlockState state ) {
-        return state.getValue( FACING ).getIndex();
-    }
-
-    @Override
-    public IBlockState getStateForPlacement( World worldIn,
-                                             BlockPos pos,
-                                             EnumFacing facing,
-                                             float hitX,
-                                             float hitY,
-                                             float hitZ,
-                                             int meta,
-                                             EntityLivingBase placer )
+    public boolean canConnectRedstone( IBlockState p_canConnectRedstone_1_,
+                                       IBlockAccess p_canConnectRedstone_2_,
+                                       BlockPos p_canConnectRedstone_3_,
+                                       @Nullable EnumFacing p_canConnectRedstone_4_ )
     {
-        return this.getDefaultState().withProperty( FACING, EnumFacing.getDirectionFromEntityLiving( pos, placer ) );
+        return true;
     }
 
     @Override
@@ -78,27 +62,80 @@ public abstract class AbstractBrightLight extends Block
     }
 
     @Override
-    public AxisAlignedBB getBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
-        switch ( state.getValue( FACING ) ) {
-            case SOUTH:
-            default:
-                return new AxisAlignedBB( 1D, 0D, 0.2D, 0D, 1D, 0D );
-            case NORTH:
-                return new AxisAlignedBB( 0D, 0D, 0.8D, 1D, 1D, 1D );
-            case WEST:
-                return new AxisAlignedBB( 0.8D, 0D, 1D, 1D, 1D, 0D );
-            case EAST:
-                return new AxisAlignedBB( 0.2D, 0D, 0D, 0D, 1D, 1D );
-            case UP:
-                return new AxisAlignedBB( 0D, 0.2D, 0D, 1D, 0D, 1D );
-            case DOWN:
-                return new AxisAlignedBB( 0D, 0.8D, 1D, 1D, 1D, 0D );
+    public void neighborChanged( IBlockState state, World world, BlockPos pos, Block blockIn, BlockPos p_189540_5_ ) {
+        boolean powered = world.isBlockPowered( pos );
+        world.setBlockState( pos, state.withProperty( POWERED, powered ), 3 );
+        // Powered, Add Light
+        if ( powered ) {
+            BlockPos doAddAt = null;
+            for ( int findy = -1; findy >= -40; findy-- ) {
+                BlockPos test = new BlockPos( pos.getX(), pos.getY() + findy, pos.getZ() );
+                IBlockState bs = world.getBlockState( test );
+                Block block = bs.getBlock();
+                // found block that is not air
+                if ( block.isAir( bs, world, test ) || block.isReplaceable( world, test ) ) {
+                    // dont add light if block is right below street light
+                    if ( findy < -1 ) {
+                        doAddAt = test;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+            // add if marked for add
+            if ( doAddAt != null ) {
+                world.setBlockState( doAddAt, Block.getBlockFromName( "csm:lightupair" ).getDefaultState(), 3 );
+            }
+        }
+        // Not powered, remove light
+        else {
+            for ( int findy = -1; findy >= -40; findy-- ) {
+                BlockPos test = new BlockPos( pos.getX(), pos.getY() + findy, pos.getZ() );
+                IBlockState bs = world.getBlockState( test );
+                // stop removing light once hit block
+                if ( bs.getBlock() == Block.getBlockFromName( "csm:lightupair" ) ) {
+                    world.setBlockToAir( test );
+                }
+            }
         }
     }
 
     @Override
+    public void onBlockPlacedBy( World world,
+                                 BlockPos pos,
+                                 IBlockState state,
+                                 EntityLivingBase placer,
+                                 ItemStack stack )
+    {
+        world.setBlockState( pos, state.withProperty( FACING, getFacingFromEntity( pos, placer ) ), 2 );
+    }
+
+    public static EnumFacing getFacingFromEntity( BlockPos clickedBlock, EntityLivingBase entity ) {
+        return EnumFacing.getFacingFromVector( ( float ) ( entity.posX - clickedBlock.getX() ),
+                                               ( float ) ( entity.posY - clickedBlock.getY() ),
+                                               ( float ) ( entity.posZ - clickedBlock.getZ() ) );
+    }
+
+    @Override
+    public IBlockState getStateFromMeta( int meta ) {
+        return getDefaultState().withProperty( FACING, EnumFacing.getFront( meta & 7 ) )
+                                .withProperty( POWERED, ( meta & 8 ) != 0 );
+    }
+
+    @Override
+    public int getMetaFromState( IBlockState state ) {
+        return state.getValue( FACING ).getIndex() + ( state.getValue( POWERED ) ? 8 : 0 );
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer( this, FACING, POWERED );
+    }
+
+    @Override
     public int getLightValue( IBlockState state, IBlockAccess world, BlockPos pos ) {
-        return 0;
+        return state.getValue( POWERED ) ? 15 : 0;
     }
 
     @Override
@@ -106,33 +143,5 @@ public abstract class AbstractBrightLight extends Block
         return false;
     }
 
-    @Override
-    public int tickRate( World p_tickRate_1_ ) {
-        return 180;
-    }
-
-    @Override
-    public void onBlockAdded( World p_onBlockAdded_1_, BlockPos p_onBlockAdded_2_, IBlockState p_onBlockAdded_3_ ) {
-        p_onBlockAdded_1_.scheduleUpdate( p_onBlockAdded_2_, this, this.tickRate( p_onBlockAdded_1_ ) );
-        super.onBlockAdded( p_onBlockAdded_1_, p_onBlockAdded_2_, p_onBlockAdded_3_ );
-    }
-
-    @Override
-    public void updateTick( World p_updateTick_1_,
-                            BlockPos p_updateTick_2_,
-                            IBlockState p_updateTick_3_,
-                            Random p_updateTick_4_ )
-    {
-        try {
-            onTick( p_updateTick_1_, p_updateTick_2_, p_updateTick_3_ );
-        }
-        catch ( Exception ignored ) {
-
-        }
-        p_updateTick_1_.scheduleUpdate( p_updateTick_2_, this, this.tickRate( p_updateTick_1_ ) );
-    }
-
     abstract public String getBlockRegistryName();
-
-    abstract public void onTick( World world, BlockPos blockPos, IBlockState blockState );
 }
