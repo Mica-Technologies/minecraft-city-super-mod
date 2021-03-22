17beaff1aa293bed609ea1057600b42f357420da
MICA_ADMIN <webmaster@micatechnologies.com>
2021-03-22 13:54:46 -0400

    add new right arrow variation

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockControllableVerticalAngleRight2Signal.java
@@ -0,0 +1,66 @@
+package com.micatechnologies.minecraft.csm.trafficsignals;
+
+import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@ElementsCitySuperMod.ModElement.Tag
+public class BlockControllableVerticalAngleRight2Signal extends ElementsCitySuperMod.ModElement
+{
+    @GameRegistry.ObjectHolder( "csm:controllableverticalangleright2signal" )
+    public static final Block block = null;
+
+    public BlockControllableVerticalAngleRight2Signal( ElementsCitySuperMod instance ) {
+        super( instance, 2774);
+    }
+
+    @Override
+    public void initElements() {
+        elements.blocks.add( () -> new BlockCustom() );
+        elements.items.add( () -> new ItemBlock( block ).setRegistryName( block.getRegistryName() ) );
+    }
+
+    @SideOnly( Side.CLIENT )
+    @Override
+    public void registerModels( ModelRegistryEvent event ) {
+        ModelLoader.setCustomModelResourceLocation( Item.getItemFromBlock( block ), 0,
+                                                    new ModelResourceLocation( "csm:controllableverticalangleright2signal",
+                                                                               "inventory" ) );
+    }
+
+    public static class BlockCustom extends AbstractBlockControllableSignal
+    {
+        public BlockCustom() {
+            super( Material.ROCK );
+            setRegistryName( "controllableverticalangleright2signal" );
+            setUnlocalizedName( "controllableverticalangleright2signal" );
+            setSoundType( SoundType.STONE );
+            setHarvestLevel( "pickaxe", 1 );
+            setHardness( 2F );
+            setResistance( 10F );
+            setLightLevel( 0F );
+            setLightOpacity( 0 );
+            setCreativeTab( TabTrafficSignals.tab );
+            this.setDefaultState(
+                    this.blockState.getBaseState().withProperty( FACING, EnumFacing.NORTH ).withProperty( COLOR, 3 ) );
+        }
+
+        @Override
+        public SIGNAL_SIDE getSignalSide( World world, BlockPos blockPos ) {
+            return SIGNAL_SIDE.RIGHT;
+        }
+
+    }
+}
