3d6d080fc544795be9d685789d2099f490a657df
Brandon Holt (RinRin Blissgreen) <rinrinblissgreen@gmail.com>
2021-05-10 12:10:24 -0700

    Fixed wrong code in BlockAltoLMM

--- src/main/java/com/micatechnologies/minecraft/csm/lighting/BlockAltoLLM.java
+++ src/main/java/com/micatechnologies/minecraft/csm/lighting/BlockAltoLLM.java
@@ -1,72 +1,134 @@
 package com.micatechnologies.minecraft.csm.lighting;
 
 import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
-import com.micatechnologies.minecraft.csm.lighting.AbstractBrightLight;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.Item;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @ElementsCitySuperMod.ModElement.Tag
-public class BlockAltoLLM extends ElementsCitySuperMod.ModElement {
-	public static final String elementId = "altollm";
+public class BlockAltoLMM extends ElementsCitySuperMod.ModElement
+{
+    @GameRegistry.ObjectHolder( "csm:altolmm" )
+    public static final Block block = null;
 
-	@GameRegistry.ObjectHolder("csm:"+elementId)
-	public static final Block block = null;
-	public BlockAltoLLM(ElementsCitySuperMod instance) {
-		super(instance, 349);
-	}
+    public BlockAltoLMM( ElementsCitySuperMod instance ) {
+        super( instance, 89 );
+    }
 
-	@Override
-	public void initElements() {
-		elements.blocks.add(() -> new BlockCustom());
-		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
-	}
+    @Override
+    public void initElements() {
+        elements.blocks.add( () -> new BlockCustom().setRegistryName( "fbm" ) );
+        elements.items.add( () -> new ItemBlock( block ).setRegistryName( block.getRegistryName() ) );
+    }
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("csm:"+elementId
-				, "inventory"));
-	}
-	public static class BlockCustom extends AbstractBrightLight
-	{
-		@Override
-		public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-			switch ((EnumFacing) state.getValue(BlockHorizontal.FACING)) {
-				case SOUTH :
-				default :
-					return new AxisAlignedBB(1D, 0D, 1D, 0D, 0.5D, 0D);
-				case NORTH :
-					return new AxisAlignedBB(0D, 0D, 0D, 1D, 0.5D, 1D);
-				case WEST :
-					return new AxisAlignedBB(0D, 0D, 1D, 1D, 0.5D, 0D);
-				case EAST :
-					return new AxisAlignedBB(1D, 0D, 0D, 0D, 0.5D, 1D);
-			}
-		}
+    @SideOnly( Side.CLIENT )
+    @Override
+    public void registerModels( ModelRegistryEvent event ) {
+        ModelLoader.setCustomModelResourceLocation( Item.getItemFromBlock( block ), 0,
+                                                    new ModelResourceLocation( "csm:fbm", "inventory" ) );
+    }
 
-		@Override
-		public String getBlockRegistryName() {
-			return elementId;
-		}
+    public static class BlockCustom extends Block
+    {
+        public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
-		@Override
-		public boolean blockHasAutomaticFunctionality() {
-			return true;
-		}
-	}
+        public BlockCustom() {
+            super( Material.ROCK );
+            setUnlocalizedName( "fbm" );
+            setSoundType( SoundType.STONE );
+            setHardness( 1F );
+            setResistance( 10F );
+            setLightLevel( 0F );
+            setLightOpacity( 0 );
+            setCreativeTab( TabLighting.tab );
+            this.setDefaultState( this.blockState.getBaseState().withProperty( FACING, EnumFacing.NORTH ) );
+        }
+
+        @Override
+        public IBlockState getStateFromMeta( int meta ) {
+            return this.getDefaultState().withProperty( FACING, EnumFacing.getFront( meta ) );
+        }
+
+        @Override
+        public int getMetaFromState( IBlockState state ) {
+            return state.getValue( FACING ).getIndex();
+        }
+
+        @Override
+        public IBlockState withRotation( IBlockState state, Rotation rot ) {
+            return state.withProperty( FACING, rot.rotate( state.getValue( FACING ) ) );
+        }
+
+        @Override
+        public IBlockState withMirror( IBlockState state, Mirror mirrorIn ) {
+            return state.withRotation( mirrorIn.toRotation( state.getValue( FACING ) ) );
+        }
+
+        @Override
+        public boolean isFullCube( IBlockState state ) {
+            return false;
+        }
+
+        @Override
+        public AxisAlignedBB getBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
+            switch ( state.getValue( BlockHorizontal.FACING ) ) {
+                case SOUTH:
+                default:
+                    return new AxisAlignedBB( 1D, 0D, 0D, 0D, 1D, 2D );
+                case NORTH:
+                    return new AxisAlignedBB( 0D, 0D, 1D, 1D, 1D, -1D );
+                case WEST:
+                    return new AxisAlignedBB( 1D, 0D, 1D, -1D, 1D, 0D );
+                case EAST:
+                    return new AxisAlignedBB( 0D, 0D, 0D, 2D, 1D, 1D );
+                case UP:
+                    return new AxisAlignedBB( 0D, 0D, 0D, 1D, 2D, 1D );
+                case DOWN:
+                    return new AxisAlignedBB( 0D, 1D, 1D, 1D, -1D, 0D );
+            }
+        }
+
+        @Override
+        public boolean isOpaqueCube( IBlockState state ) {
+            return false;
+        }
+
+        @Override
+        public IBlockState getStateForPlacement( World worldIn,
+                                                 BlockPos pos,
+                                                 EnumFacing facing,
+                                                 float hitX,
+                                                 float hitY,
+                                                 float hitZ,
+                                                 int meta,
+                                                 EntityLivingBase placer )
+        {
+            return this.getDefaultState()
+                       .withProperty( FACING, EnumFacing.getDirectionFromEntityLiving( pos, placer ) );
+        }
+
+        @Override
+        protected net.minecraft.block.state.BlockStateContainer createBlockState() {
+            return new net.minecraft.block.state.BlockStateContainer( this, FACING );
+        }
+    }
 }
-
