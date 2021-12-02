0e7c80c8aabac72e73371e898b5b5f35bfc7aecd
mica-alex <83238954+mica-alex@users.noreply.github.com>
2021-12-02 16:09:16 -0500

    Add bell sensor

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockTrafficLightSensorBell.java
@@ -0,0 +1,57 @@
+package com.micatechnologies.minecraft.csm.trafficsignals;
+
+import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import com.micatechnologies.minecraft.csm.trafficsignals.logic.AbstractBlockTrafficSignalSensor;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@ElementsCitySuperMod.ModElement.Tag
+public class BlockTrafficLightSensorBell extends ElementsCitySuperMod.ModElement
+{
+    @GameRegistry.ObjectHolder( "csm:trafficlightsensorbell" )
+    public static final Block block = null;
+
+    public BlockTrafficLightSensorBell( ElementsCitySuperMod instance ) {
+        super( instance, 2245 );
+    }
+
+    @Override
+    public void initElements() {
+        elements.blocks.add( () -> new BlockCustom().setRegistryName( "trafficlightsensorbell" ) );
+        elements.items.add( () -> new ItemBlock( block ).setRegistryName( block.getRegistryName() ) );
+    }
+
+    @SideOnly( Side.CLIENT )
+    @Override
+    public void registerModels( ModelRegistryEvent event ) {
+        ModelLoader.setCustomModelResourceLocation( Item.getItemFromBlock( block ), 0,
+                                                    new ModelResourceLocation( "csm:trafficlightsensorbell",
+                                                                               "inventory" ) );
+    }
+
+    public static class BlockCustom extends AbstractBlockTrafficSignalSensor
+    {
+        public BlockCustom() {
+            super( Material.ROCK );
+            setUnlocalizedName( "trafficlightsensorbell" );
+            setSoundType( SoundType.STONE );
+            setHarvestLevel( "pickaxe", 1 );
+            setHardness( 2F );
+            setResistance( 10F );
+            setLightLevel( 0F );
+            setLightOpacity( 0 );
+            setCreativeTab( TabTrafficSignals.tab );
+            this.setDefaultState( this.blockState.getBaseState().withProperty( FACING, EnumFacing.NORTH ) );
+        }
+    }
+}
