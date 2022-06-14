db26b78baf0f612db9b948e86c0011411f74a012
mica-alex <83238954+mica-alex@users.noreply.github.com>
2022-06-14 11:00:13 -0400

    Add new sensor

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockTrafficLightSensorModern.java
@@ -0,0 +1,64 @@
+package com.micatechnologies.minecraft.csm.trafficsignals;
+
+import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import com.micatechnologies.minecraft.csm.trafficsignals.logic.AbstractBlockTrafficSignalSensor;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@ElementsCitySuperMod.ModElement.Tag
+public class BlockTrafficLightSensorModern extends ElementsCitySuperMod.ModElement
+{
+    @GameRegistry.ObjectHolder( "csm:trafficlightsensormodern" )
+    public static final Block block = null;
+
+    public BlockTrafficLightSensorModern( ElementsCitySuperMod instance ) {
+        super( instance, 654 );
+    }
+
+    @Override
+    public void initElements() {
+        elements.blocks.add( () -> new BlockCustom().setRegistryName( "trafficlightsensormodern" ) );
+        elements.items.add( () -> new ItemBlock( block ).setRegistryName( block.getRegistryName() ) );
+    }
+
+    @Override
+    public void init( FMLInitializationEvent event ) {
+        GameRegistry.registerTileEntity( TileEntityTrafficSignalSensor.class,
+                                         "csm" + ":tileentitytrafficsignalsensor" );
+    }
+
+    @SideOnly( Side.CLIENT )
+    @Override
+    public void registerModels( ModelRegistryEvent event ) {
+        ModelLoader.setCustomModelResourceLocation( Item.getItemFromBlock( block ), 0,
+                                                    new ModelResourceLocation( "csm:trafficlightsensormodern",
+                                                                               "inventory" ) );
+    }
+
+    public static class BlockCustom extends AbstractBlockTrafficSignalSensor
+    {
+        public BlockCustom() {
+            super( Material.ROCK );
+            setUnlocalizedName( "trafficlightsensormodern" );
+            setSoundType( SoundType.STONE );
+            setHarvestLevel( "pickaxe", 1 );
+            setHardness( 2F );
+            setResistance( 10F );
+            setLightLevel( 0F );
+            setLightOpacity( 0 );
+            setCreativeTab( TabTrafficSignals.tab );
+            this.setDefaultState( this.blockState.getBaseState().withProperty( FACING, EnumFacing.NORTH ) );
+        }
+    }
+}
