95ff0945073ae333a0c78e07a15ef2b2d8a15785
Brandon Holt (RinRin Blissgreen) <rinrinblissgreen@gmail.com>
2022-07-21 21:15:32 -0700

    More grid stuff and sign updates

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/BlockLHSStopSign.java
@@ -0,0 +1,55 @@
+
+package com.micatechnologies.minecraft.csm.trafficsigns;
+
+import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockDirectional;
+import net.minecraft.block.Block;
+
+@ElementsCitySuperMod.ModElement.Tag
+public class BlockLHSStopSign extends ElementsCitySuperMod.ModElement {
+	@GameRegistry.ObjectHolder("csm:lhsstopsign")
+	public static final Block block = null;
+	public BlockLHSStopSign(ElementsCitySuperMod instance) {
+		super(instance, 257);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("lhsstopsign"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("csm:lhsstopsign", "inventory"));
+	}
+	public static class BlockCustom extends AbstractBlockSign
+    {
+        @Override
+        public String getBlockRegistryName() {
+            return "lhsstopsign";
+        }
+	}
+}
