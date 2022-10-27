b38f801a8088c62b813a4ff9ad2125d03b3d8ef6
mica-alex <83238954+mica-alex@users.noreply.github.com>
2022-10-27 12:22:22 -0400

    Add second APS model

--- src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockControllableCrosswalkButtonMale.java
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockControllableCrosswalkButtonMale.java
@@ -7,25 +7,18 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-import java.util.Random;
 
 @ElementsCitySuperMod.ModElement.Tag
 public class BlockControllableCrosswalkButtonMale extends ElementsCitySuperMod.ModElement
@@ -59,9 +52,6 @@ public class BlockControllableCrosswalkButtonMale extends ElementsCitySuperMod.M
 
     public static class BlockCustom extends AbstractBlockTrafficSignalRequester
     {
-        final int lenOfWalkSound           = 140;
-        final int lenOfDontWalkSound       = 30;
-        final int lenOfNotSafeToCrossSound = 140;
 
         public BlockCustom() {
             super( Material.ROCK );
@@ -83,8 +73,6 @@ public class BlockControllableCrosswalkButtonMale extends ElementsCitySuperMod.M
             return 0;
         }
 
-        private int lastColor = 0;
-
         @Override
         public AxisAlignedBB getBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
             switch ( state.getValue( FACING ) ) {
@@ -102,76 +90,6 @@ public class BlockControllableCrosswalkButtonMale extends ElementsCitySuperMod.M
                 case DOWN:
                     return new AxisAlignedBB( 0D, 0.8D, 1D, 1D, 1D, 0D );
             }
-        }
-
-        @Override
-        public boolean onBlockActivated( World p_onBlockActivated_1_,
-                                         BlockPos p_onBlockActivated_2_,
-                                         IBlockState p_onBlockActivated_3_,
-                                         EntityPlayer p_onBlockActivated_4_,
-                                         EnumHand p_onBlockActivated_5_,
-                                         EnumFacing p_onBlockActivated_6_,
-                                         float p_onBlockActivated_7_,
-                                         float p_onBlockActivated_8_,
-                                         float p_onBlockActivated_9_ )
-        {
-            if ( p_onBlockActivated_3_.getValue( COLOR ) == 0 ) {
-                p_onBlockActivated_1_.playSound( null, p_onBlockActivated_2_.getX(), p_onBlockActivated_2_.getY(),
-                                                 p_onBlockActivated_2_.getZ(),
-                                                 net.minecraft.util.SoundEvent.REGISTRY.getObject(
-                                                         new ResourceLocation( "csm" + ":male_wait" ) ),
-                                                 SoundCategory.NEUTRAL, ( float ) 2, ( float ) 1 );
-            }
-
-            return super.onBlockActivated( p_onBlockActivated_1_, p_onBlockActivated_2_, p_onBlockActivated_3_,
-                                           p_onBlockActivated_4_, p_onBlockActivated_5_, p_onBlockActivated_6_,
-                                           p_onBlockActivated_7_, p_onBlockActivated_8_, p_onBlockActivated_9_ );
-        }
-
-        @Override
-        public void updateTick( World p_updateTick_1_,
-                                BlockPos p_updateTick_2_,
-                                IBlockState p_updateTick_3_,
-                                Random p_updateTick_4_ )
-        {
-            int color = p_updateTick_3_.getValue( COLOR );
-            if ( color == 0 ) {
-                // Play beep
-                p_updateTick_1_.playSound( null, p_updateTick_2_.getX(), p_updateTick_2_.getY(), p_updateTick_2_.getZ(),
-                                           net.minecraft.util.SoundEvent.REGISTRY.getObject(
-                                                   new ResourceLocation( "csm:male_beep" ) ), SoundCategory.NEUTRAL,
-                                           ( float ) 2, ( float ) 1 );
-
-                p_updateTick_1_.scheduleUpdate( p_updateTick_2_, this, lenOfDontWalkSound );
-            }
-            else if ( color == 2 ) {
-                // Play walk voice
-                p_updateTick_1_.playSound( null, p_updateTick_2_.getX(), p_updateTick_2_.getY(), p_updateTick_2_.getZ(),
-                                           net.minecraft.util.SoundEvent.REGISTRY.getObject(
-                                                   new ResourceLocation( "csm:male_crosswalk_on" ) ),
-                                           SoundCategory.NEUTRAL, ( float ) 2, ( float ) 1 );
-
-                p_updateTick_1_.scheduleUpdate( p_updateTick_2_, this, lenOfWalkSound );
-            }
-            else {
-                p_updateTick_1_.scheduleUpdate( p_updateTick_2_, this, this.tickRate( p_updateTick_3_ ) );
-            }
-
-            lastColor = color;
-        }
-
-        @Override
-        public void onBlockAdded( World p_onBlockAdded_1_, BlockPos p_onBlockAdded_2_, IBlockState p_onBlockAdded_3_ ) {
-            p_onBlockAdded_1_.scheduleUpdate( p_onBlockAdded_2_, this, this.tickRate( p_onBlockAdded_3_ ) );
-            super.onBlockAdded( p_onBlockAdded_1_, p_onBlockAdded_2_, p_onBlockAdded_3_ );
-        }
-
-        public int tickRate( IBlockState p_tickRate_1_ ) {
-            int len = lenOfWalkSound;
-            if ( p_tickRate_1_.getValue( COLOR ) == 0 ) {
-                len = lenOfDontWalkSound;
-            }
-            return len;
         }
     }
 }
