8e20c210f6406300b5c45de32a6402d2e6bacc73
mica-alex <83238954+mica-alex@users.noreply.github.com>
2022-12-29 12:25:02 -0500

    Add infinite FE power block

---
+++ src/main/java/com/micatechnologies/minecraft/csm/powergrid/fe/BlockForgeEnergyProducer.java
@@ -0,0 +1,150 @@
+package com.micatechnologies.minecraft.csm.powergrid.fe;
+
+import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import com.micatechnologies.minecraft.csm.powergrid.TabPowerGrid;
+import com.micatechnologies.minecraft.csm.trafficsignals.ItemEWSignalLinker;
+import com.micatechnologies.minecraft.csm.trafficsignals.ItemNSSignalLinker;
+import com.micatechnologies.minecraft.csm.trafficsignals.TileEntityTrafficSignalController;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+@ElementsCitySuperMod.ModElement.Tag
+public class BlockForgeEnergyProducer extends ElementsCitySuperMod.ModElement
+{
+    @GameRegistry.ObjectHolder( "csm:rfprod" )
+    public static final Block block = null;
+
+    public BlockForgeEnergyProducer( ElementsCitySuperMod instance ) {
+        super( instance, 1879 );
+    }
+
+    @Override
+    public void initElements() {
+        elements.blocks.add( () -> new BlockCustom().setRegistryName( "rfprod" ) );
+        elements.items.add( () -> new ItemBlock( block ).setRegistryName( block.getRegistryName() ) );
+    }
+
+    @Override
+    public void init( FMLInitializationEvent event ) {
+        GameRegistry.registerTileEntity( TileEntityForgeEnergyProducer.class, "csm:tileentityforgeenergyproducer" );
+    }
+
+    @SideOnly( Side.CLIENT )
+    @Override
+    public void registerModels( ModelRegistryEvent event ) {
+        ModelLoader.setCustomModelResourceLocation( Item.getItemFromBlock( block ), 0,
+                                                    new ModelResourceLocation( "csm:rfprod", "inventory" ) );
+    }
+
+    public static class BlockCustom extends Block implements ITileEntityProvider
+    {
+
+        public BlockCustom() {
+            super( Material.ROCK );
+            setUnlocalizedName( "rfprod" );
+            setSoundType( SoundType.STONE );
+            setHarvestLevel( "pickaxe", 1 );
+            setHardness( 2F );
+            setResistance( 10F );
+            setLightLevel( 0F );
+            setLightOpacity( 0 );
+            setCreativeTab( TabPowerGrid.tab );
+        }
+
+        @Override
+        @ParametersAreNonnullByDefault
+        public boolean isOpaqueCube( IBlockState state ) {
+            return false;
+        }
+
+        @Override
+        @ParametersAreNonnullByDefault
+        public boolean canProvidePower( IBlockState p_canProvidePower_1_ ) {
+            return true;
+        }
+
+        @Override
+        @ParametersAreNonnullByDefault
+        public void addInformation( ItemStack p_addInformation_1_,
+                                    World p_addInformation_2_,
+                                    List< String > p_addInformation_3_,
+                                    ITooltipFlag p_addInformation_4_ )
+        {
+            super.addInformation( p_addInformation_1_, p_addInformation_2_, p_addInformation_3_, p_addInformation_4_ );
+            p_addInformation_3_.add( I18n.format( "csm.highvoltage" ) );
+        }
+
+        @Override
+        @ParametersAreNonnullByDefault
+        public TileEntity createNewTileEntity( World world, int i ) {
+            return new TileEntityForgeEnergyProducer();
+        }
+
+        @Override
+        public boolean onBlockActivated( World p_onBlockActivated_1_,
+                                         BlockPos p_onBlockActivated_2_,
+                                         IBlockState p_onBlockActivated_3_,
+                                         EntityPlayer p_onBlockActivated_4_,
+                                         EnumHand p_onBlockActivated_5_,
+                                         EnumFacing p_onBlockActivated_6_,
+                                         float p_onBlockActivated_7_,
+                                         float p_onBlockActivated_8_,
+                                         float p_onBlockActivated_9_ )
+        {
+            // Increment tick rate if tile entity is present and valid
+            if ( p_onBlockActivated_4_.isSneaking() ) {
+                TileEntity tileEntity = p_onBlockActivated_1_.getTileEntity( p_onBlockActivated_2_ );
+                if ( tileEntity instanceof TileEntityForgeEnergyProducer ) {
+                    TileEntityForgeEnergyProducer tileEntityForgeEnergyProducer
+                            = ( TileEntityForgeEnergyProducer ) tileEntity;
+                    int tickRate = tileEntityForgeEnergyProducer.incrementTickRate();
+                    if ( !p_onBlockActivated_1_.isRemote ) {
+                        double tickRateSeconds = ( double ) tickRate / 20.0;
+                        p_onBlockActivated_4_.sendMessage( new TextComponentString(
+                                "Producer infinite output rate set to " +
+                                        tickRate +
+                                        " ticks" +
+                                        " (" +
+                                        tickRateSeconds +
+                                        " seconds)." ) );
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        public boolean hasTileEntity( IBlockState p_hasTileEntity_1_ ) {
+            return true;
+        }
+    }
+}
---
+++ src/main/java/com/micatechnologies/minecraft/csm/powergrid/fe/TileEntityForgeEnergyProducer.java
@@ -0,0 +1,154 @@
+package com.micatechnologies.minecraft.csm.powergrid.fe;
+
+import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+
+import javax.annotation.Nullable;
+
+@ElementsCitySuperMod.ModElement.Tag
+public class TileEntityForgeEnergyProducer extends TileEntity implements IEnergyStorage, ITickable
+{
+    private static final String NBT_TICK_RATE_KEY = "tickRate";
+    private              int    tickRate          = 1;
+
+    @Override
+    public void readFromNBT( NBTTagCompound p_readFromNBT_1_ ) {
+        super.readFromNBT( p_readFromNBT_1_ );
+        if ( p_readFromNBT_1_.hasKey( NBT_TICK_RATE_KEY ) ) {
+            tickRate = p_readFromNBT_1_.getInteger( NBT_TICK_RATE_KEY );
+        }
+        else {
+            tickRate = 1;
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT( NBTTagCompound p_writeToNBT_1_ ) {
+        p_writeToNBT_1_.setInteger( NBT_TICK_RATE_KEY, tickRate );
+        return super.writeToNBT( p_writeToNBT_1_ );
+    }
+
+    @Override
+    public boolean hasCapability( Capability< ? > p_hasCapability_1_, @Nullable EnumFacing p_hasCapability_2_ )
+    {
+        return p_hasCapability_1_ == CapabilityEnergy.ENERGY ||
+                super.hasCapability( p_hasCapability_1_, p_hasCapability_2_ );
+    }
+
+    @Nullable
+    @Override
+    public < T > T getCapability( Capability< T > p_getCapability_1_, @Nullable EnumFacing p_getCapability_2_ )
+    {
+        return p_getCapability_1_ == CapabilityEnergy.ENERGY ?
+               CapabilityEnergy.ENERGY.cast( this ) :
+               super.getCapability( p_getCapability_1_, p_getCapability_2_ );
+    }
+
+    @Override
+    public synchronized int receiveEnergy( int i, boolean b ) {
+        return 0;
+    }
+
+    @Override
+    public int extractEnergy( int i, boolean b ) {
+        return i;
+    }
+
+    @Override
+    public int getEnergyStored() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public int getMaxEnergyStored() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean canExtract() {
+        return true;
+    }
+
+    @Override
+    public boolean canReceive() {
+        return false;
+    }
+
+    public int incrementTickRate() {
+        tickRate += 5;
+        if ( tickRate > 80 ) {
+            tickRate = 1;
+        }
+        markDirty();
+        return tickRate;
+    }
+
+    @Override
+    public void update() {
+        // This is called every tick, need to check if it is time to act
+        if ( !getWorld().isRemote && getWorld().getTotalWorldTime() % tickRate == 0L ) {
+            try {
+                // Only provide power if redstone enabled
+                if ( getWorld().isBlockPowered( pos ) ) {
+                    for ( EnumFacing facing : EnumFacing.values() ) {
+                        BlockPos offset = pos.offset( facing );
+                        TileEntity tileEntity = world.getTileEntity( offset );
+                        if ( tileEntity != null ) {
+                            if ( tileEntity.hasCapability( CapabilityEnergy.ENERGY, facing.getOpposite() ) ) {
+                                IEnergyStorage energyStorage = tileEntity.getCapability( CapabilityEnergy.ENERGY,
+                                                                                         facing.getOpposite() );
+                                if ( energyStorage != null ) {
+                                    energyStorage.receiveEnergy( Integer.MAX_VALUE, false );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch ( Exception e ) {
+                System.err.println( "An error occurred while ticking a Forge energy infinite producer: " );
+                e.printStackTrace( System.err );
+            }
+        }
+    }
+
+    @Override
+    @Nullable
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        NBTTagCompound nbtTagCompound = new NBTTagCompound();
+        writeToNBT( nbtTagCompound );
+        int metadata = getBlockMetadata();
+        return new SPacketUpdateTileEntity( this.pos, metadata, nbtTagCompound );
+    }
+
+    @Override
+    public void onDataPacket( NetworkManager networkManager, SPacketUpdateTileEntity pkt ) {
+        readFromNBT( pkt.getNbtCompound() );
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        NBTTagCompound nbtTagCompound = new NBTTagCompound();
+        writeToNBT( nbtTagCompound );
+        return nbtTagCompound;
+    }
+
+    @Override
+    public void handleUpdateTag( NBTTagCompound nbtTagCompound )
+    {
+        this.readFromNBT( nbtTagCompound );
+    }
+}
