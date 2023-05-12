505a9f956fdf0fa8753b32429eaecc1c00bf5e3e
mica-alex <83238954+mica-alex@users.noreply.github.com>
2023-05-12 13:49:58 -0400

    migrate tts block to new TE

--- src/main/java/com/micatechnologies/minecraft/csm/technology/TileEntityRedstoneTTS.java
+++ src/main/java/com/micatechnologies/minecraft/csm/technology/TileEntityRedstoneTTS.java
@@ -1,21 +1,14 @@
 package com.micatechnologies.minecraft.csm.technology;
 
 import com.micatechnologies.minecraft.csm.ElementsCitySuperMod;
-import net.minecraft.block.state.IBlockState;
+import com.micatechnologies.minecraft.csm.codeutils.AbstractTileEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SPacketUpdateTileEntity;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import com.sun.speech.freetts.*;
 
-import javax.annotation.Nullable;
-
 @ElementsCitySuperMod.ModElement.Tag
-public class TileEntityRedstoneTTS extends TileEntity
+public class TileEntityRedstoneTTS extends AbstractTileEntity
 {
     private static final String TTS_STRING_KEY     = "ttsString";
     private static final String TTS_VOICE_NAME_KEY = "ttsVoice";
@@ -25,64 +18,37 @@ public class TileEntityRedstoneTTS extends TileEntity
     private              String ttsVoiceName                  = "kevin16";
     private              float  ttsVoiceDurationStretch       = 1f;
 
+    /**
+     * Returns the NBT tag compound with the tile entity's NBT data.
+     *
+     * @param compound the NBT tag compound to write the tile entity's NBT data to
+     *
+     * @return the NBT tag compound with the tile entity's NBT data
+     */
     @Override
-    public void readFromNBT( NBTTagCompound p_readFromNBT_1_ ) {
-        super.readFromNBT( p_readFromNBT_1_ );
-        if ( p_readFromNBT_1_.hasKey( TTS_STRING_KEY ) ) {
-            ttsString = p_readFromNBT_1_.getString( TTS_STRING_KEY );
+    public NBTTagCompound writeNBT( NBTTagCompound compound ) {
+        compound.setString( TTS_STRING_KEY, ttsString );
+        compound.setString( TTS_VOICE_NAME_KEY, ttsVoiceName );
+        compound.setFloat( TTS_VOICE_DURATION_SRETCH_KEY, ttsVoiceDurationStretch );
+        return compound;
+    }
+
+    /**
+     * Processes the reading of the tile entity's NBT data from the supplied NBT tag compound.
+     *
+     * @param compound the NBT tag compound to read the tile entity's NBT data from
+     */
+    @Override
+    public void readNBT( NBTTagCompound compound ) {
+        if ( compound.hasKey( TTS_STRING_KEY ) ) {
+            ttsString = compound.getString( TTS_STRING_KEY );
         }
-        if ( p_readFromNBT_1_.hasKey( TTS_VOICE_NAME_KEY ) ) {
-            ttsVoiceName = p_readFromNBT_1_.getString( TTS_VOICE_NAME_KEY );
+        if ( compound.hasKey( TTS_VOICE_NAME_KEY ) ) {
+            ttsVoiceName = compound.getString( TTS_VOICE_NAME_KEY );
         }
-        if ( p_readFromNBT_1_.hasKey( TTS_VOICE_DURATION_SRETCH_KEY ) ) {
-            ttsVoiceDurationStretch = p_readFromNBT_1_.getFloat( TTS_VOICE_DURATION_SRETCH_KEY );
+        if ( compound.hasKey( TTS_VOICE_DURATION_SRETCH_KEY ) ) {
+            ttsVoiceDurationStretch = compound.getFloat( TTS_VOICE_DURATION_SRETCH_KEY );
         }
-    }
-
-    @Override
-    public NBTTagCompound writeToNBT( NBTTagCompound p_writeToNBT_1_ ) {
-        p_writeToNBT_1_.setString( TTS_STRING_KEY, ttsString );
-        p_writeToNBT_1_.setString( TTS_VOICE_NAME_KEY, ttsVoiceName );
-        p_writeToNBT_1_.setFloat( TTS_VOICE_DURATION_SRETCH_KEY, ttsVoiceDurationStretch );
-        return super.writeToNBT( p_writeToNBT_1_ );
-    }
-
-    @Override
-    public boolean shouldRefresh( World p_shouldRefresh_1_,
-                                  BlockPos p_shouldRefresh_2_,
-                                  IBlockState p_shouldRefresh_3_,
-                                  IBlockState p_shouldRefresh_4_ )
-    {
-        return false;
-    }
-
-    @Override
-    @Nullable
-    public SPacketUpdateTileEntity getUpdatePacket()
-    {
-        NBTTagCompound nbtTagCompound = new NBTTagCompound();
-        writeToNBT( nbtTagCompound );
-        int metadata = getBlockMetadata();
-        return new SPacketUpdateTileEntity( this.pos, metadata, nbtTagCompound );
-    }
-
-    @Override
-    public void onDataPacket( NetworkManager networkManager, SPacketUpdateTileEntity pkt ) {
-        readFromNBT( pkt.getNbtCompound() );
-    }
-
-    @Override
-    public NBTTagCompound getUpdateTag()
-    {
-        NBTTagCompound nbtTagCompound = new NBTTagCompound();
-        writeToNBT( nbtTagCompound );
-        return nbtTagCompound;
-    }
-
-    @Override
-    public void handleUpdateTag( NBTTagCompound nbtTagCompound )
-    {
-        this.readFromNBT( nbtTagCompound );
     }
 
     public void readTtsString() {
@@ -96,7 +62,7 @@ public class TileEntityRedstoneTTS extends TileEntity
 
                 // This part actually reads the text
                 ttsVoice.setVolume( Minecraft.getMinecraft().gameSettings.getSoundLevel( SoundCategory.BLOCKS ) );
-                //ttsVoice.setDurationStretch( ttsVoiceDurationStretch );
+                ttsVoice.setDurationStretch( ttsVoiceDurationStretch );
                 ttsVoice.speak( ttsString );
 
                 // Deallocate the data
@@ -115,10 +81,14 @@ public class TileEntityRedstoneTTS extends TileEntity
 
     public void setTtsString( String ttsString ) {
         this.ttsString = ttsString;
-        System.err.println("Trig time:"+System.currentTimeMillis());
-        markDirty();
-        world.scheduleBlockUpdate( pos, this.getBlockType(), 0, 0 );
-        world.markAndNotifyBlock( pos, null, world.getBlockState( pos ),world.getBlockState( pos ),3 );
+        System.err.println( "Trig time:" + System.currentTimeMillis() );
+        markDirtySync( getWorld(), getPos(),true );
+        System.out.println( "setTtsString (remote: " +
+                                    getWorld().isRemote +
+                                    ") at pos " +
+                                    getPos() +
+                                    " with string: " +
+                                    ttsString );
     }
 
     public float getTtsVoiceDurationStretch() {
@@ -127,9 +97,7 @@ public class TileEntityRedstoneTTS extends TileEntity
 
     public void setTtsVoiceDurationStretch( float ttsVoiceDurationStretch ) {
         this.ttsVoiceDurationStretch = ttsVoiceDurationStretch;
-        markDirty();
-        world.scheduleBlockUpdate( pos, this.getBlockType(), 0, 0 );
-        world.markAndNotifyBlock( pos, null, world.getBlockState( pos ),world.getBlockState( pos ),3 );
+        markDirtySync( getWorld(), getPos(),true );
     }
 }
 
