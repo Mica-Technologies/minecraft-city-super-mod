c6410b7045b9c7c3b7b31d8e5e25adde84274512
mica-alex <83238954+mica-alex@users.noreply.github.com>
2023-05-16 17:55:59 -0400

    Migrate signs to horizontal only

--- src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/AbstractBlockSign.java
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/AbstractBlockSign.java
@@ -1,13 +1,16 @@
 package com.micatechnologies.minecraft.csm.trafficsigns;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Mirror;
@@ -17,9 +20,10 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+@MethodsReturnNonnullByDefault
 public abstract class AbstractBlockSign extends Block
 {
-    public static final PropertyDirection FACING = BlockDirectional.FACING;
+    public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
     public AbstractBlockSign() {
         super( Material.ROCK );
@@ -42,40 +46,40 @@ public abstract class AbstractBlockSign extends Block
 
     @Override
     protected net.minecraft.block.state.BlockStateContainer createBlockState() {
-        return new net.minecraft.block.state.BlockStateContainer( this, new IProperty[]{ FACING } );
-    }
-
-    @Override
-    public IBlockState withRotation( IBlockState state, Rotation rot ) {
-        return state.withProperty( FACING, rot.rotate( ( EnumFacing ) state.getValue( FACING ) ) );
-    }
-
-    @Override
-    public IBlockState withMirror( IBlockState state, Mirror mirrorIn ) {
-        return state.withRotation( mirrorIn.toRotation( ( EnumFacing ) state.getValue( FACING ) ) );
+        return new net.minecraft.block.state.BlockStateContainer( this, FACING );
     }
 
     @Override
     public IBlockState getStateFromMeta( int meta ) {
-        return this.getDefaultState().withProperty( FACING, EnumFacing.getFront( meta ) );
+        int facingVal = meta;
+        // Convert old directional WEST to new horizontal WEST
+        if ( facingVal == 4 ) {
+            facingVal = 1;
+        }
+        // Convert old directional EAST to new horizontal EAST
+        else if ( facingVal == 5 ) {
+            facingVal = 3;
+        }
+        // Otherwise, fallback to SOUTH (0) if invalid
+        else if ( facingVal < 0 || facingVal > 3 ) {
+            facingVal = 0;
+        }
+        return getDefaultState().withProperty( FACING, EnumFacing.getHorizontal( facingVal ) );
     }
 
     @Override
     public int getMetaFromState( IBlockState state ) {
-        return ( ( EnumFacing ) state.getValue( FACING ) ).getIndex();
+        return state.getValue( FACING ).getHorizontalIndex();
     }
 
     @Override
-    public IBlockState getStateForPlacement( World worldIn,
-                                             BlockPos pos,
-                                             EnumFacing facing,
-                                             float hitX,
-                                             float hitY,
-                                             float hitZ,
-                                             int meta,
-                                             EntityLivingBase placer )
+    public void onBlockPlacedBy( World world,
+                                 BlockPos pos,
+                                 IBlockState state,
+                                 EntityLivingBase placer,
+                                 ItemStack stack )
     {
-        return this.getDefaultState().withProperty( FACING, EnumFacing.getDirectionFromEntityLiving( pos, placer ) );
+        world.setBlockState( pos, state.withProperty( FACING, placer.getHorizontalFacing().getOpposite() ), 2 );
     }
 
     @Override
