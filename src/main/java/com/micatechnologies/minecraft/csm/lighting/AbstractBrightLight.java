2a7097e14f95630d9c18d52f91e69342ab44cf1f
mica-alex <83238954+mica-alex@users.noreply.github.com>
2023-08-21 20:08:00 -0400

    Stage 5.1 of major restructure

--- src/main/java/com/micatechnologies/minecraft/csm/lighting/AbstractBrightLight.java
+++ src/main/java/com/micatechnologies/minecraft/csm/lighting/AbstractBrightLight.java
@@ -1,5 +1,7 @@
 package com.micatechnologies.minecraft.csm.lighting;
 
+import com.micatechnologies.minecraft.csm.codeutils.AbstractBlock;
+import com.micatechnologies.minecraft.csm.codeutils.AbstractBlockRotatableNSEW;
 import com.micatechnologies.minecraft.csm.tabs.CsmTabLighting;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockHorizontal;
@@ -15,6 +17,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.IBlockAccess;
@@ -22,37 +25,98 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public abstract class AbstractBrightLight extends Block
+public abstract class AbstractBrightLight extends AbstractBlockRotatableNSEW
 {
-    public static final  PropertyDirection FACING        = BlockHorizontal.FACING;
-    public static final  PropertyInteger   STATE         = PropertyInteger.create( "state", 0, 3 );
-    private static final int               STATE_RS_OFF  = 0;
-    private static final int               STATE_RS_ON   = 1;
-    private static final int               STATE_MAN_OFF = 2;
-    private static final int               STATE_MAN_ON  = 3;
+    public static final  PropertyInteger STATE         = PropertyInteger.create( "state", 0, 3 );
+    private static final int             STATE_RS_OFF  = 0;
+    private static final int             STATE_RS_ON   = 1;
+    private static final int             STATE_MAN_OFF = 2;
+    private static final int             STATE_MAN_ON  = 3;
 
     public AbstractBrightLight() {
-        super( Material.ROCK );
-        setRegistryName( getBlockRegistryName() );
-        setUnlocalizedName( getBlockRegistryName() );
-        setSoundType( SoundType.STONE );
-        setHarvestLevel( "pickaxe", 1 );
-        setHardness( 2F );
-        setResistance( 10F );
-        setLightLevel( 0F );
-        setLightOpacity( 0 );
-        setCreativeTab( CsmTabLighting.get() );
+        super( Material.ROCK, SoundType.STONE, "pickaxe", 1, 2F, 10F, 0F, 0, false );
         this.setDefaultState( this.blockState.getBaseState()
                                              .withProperty( FACING, EnumFacing.NORTH )
                                              .withProperty( STATE, STATE_RS_OFF ) );
     }
 
-    abstract public String getBlockRegistryName();
+    /**
+     * Retrieves whether the block is an opaque cube.
+     *
+     * @param state The block state.
+     *
+     * @return {@code true} if the block is an opaque cube, {@code false} otherwise.
+     *
+     * @since 1.0
+     */
+    @Override
+    public boolean getBlockIsOpaqueCube( IBlockState state ) {
+        return false;
+    }
+
+    /**
+     * Retrieves whether the block is a full cube.
+     *
+     * @param state The block state.
+     *
+     * @return {@code true} if the block is a full cube, {@code false} otherwise.
+     *
+     * @since 1.0
+     */
+    @Override
+    public boolean getBlockIsFullCube( IBlockState state ) {
+        return false;
+    }
+
+    /**
+     * Retrieves whether the block connects to redstone.
+     *
+     * @param state  the block state
+     * @param access the block access
+     * @param pos    the block position
+     * @param facing the block facing direction
+     *
+     * @return {@code true} if the block connects to redstone, {@code false} otherwise.
+     *
+     * @since 1.0
+     */
+    @Override
+    public boolean getBlockConnectsRedstone( IBlockState state,
+                                             IBlockAccess access,
+                                             BlockPos pos,
+                                             @Nullable EnumFacing facing )
+    {
+        return true;
+    }
+
+    /**
+     * Retrieves the block's render layer.
+     *
+     * @return The block's render layer.
+     *
+     * @since 1.0
+     */
+    @Nonnull
+    @Override
+    public BlockRenderLayer getBlockRenderLayer() {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
 
     abstract public int getBrightLightXOffset();
 
+    /**
+     * Gets the {@link IBlockState} equivalent for this block using the specified {@code meta} value.
+     *
+     * @param meta the value to get the equivalent {@link IBlockState} of
+     *
+     * @return the {@link IBlockState} equivalent for the specified {@code meta} value
+     *
+     * @see Block#getStateFromMeta(int)
+     * @since 1.0
+     */
     @Override
     public IBlockState getStateFromMeta( int meta ) {
         int stateVal = meta % 4;
@@ -62,21 +126,21 @@ public abstract class AbstractBrightLight extends Block
                                 .withProperty( STATE, stateVal );
     }
 
+    /**
+     * Gets the equivalent {@link Integer} meta value for the specified {@link IBlockState} of this block.
+     *
+     * @param state the {@link IBlockState} to get the equivalent {@link Integer} meta value for
+     *
+     * @return the equivalent {@link Integer} meta value for the specified {@link IBlockState}
+     *
+     * @see Block#getMetaFromState(IBlockState)
+     * @since 1.0
+     */
     @Override
     public int getMetaFromState( IBlockState state ) {
         int facingVal = state.getValue( FACING ).getHorizontalIndex() * 4;
         int stateVal = state.getValue( STATE );
         return facingVal + stateVal;
-    }
-
-    @Override
-    public boolean isFullCube( IBlockState state ) {
-        return false;
-    }
-
-    @Override
-    public boolean isOpaqueCube( IBlockState state ) {
-        return false;
     }
 
     @Override
@@ -149,12 +213,6 @@ public abstract class AbstractBrightLight extends Block
         super.breakBlock( p_breakBlock_1_, p_breakBlock_2_, p_breakBlock_3_ );
     }
 
-    @SideOnly( Side.CLIENT )
-    @Override
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.CUTOUT_MIPPED;
-    }
-
     @Override
     public boolean onBlockActivated( World world,
                                      BlockPos pos,
@@ -197,19 +255,18 @@ public abstract class AbstractBrightLight extends Block
         return true;
     }
 
+    /**
+     * Creates a new {@link BlockStateContainer} for the block with the required properties for rotation and state.
+     *
+     * @return a new {@link BlockStateContainer} for the block
+     *
+     * @see Block#createBlockState()
+     * @since 1.0
+     */
     @Override
-    public void onBlockPlacedBy( World world,
-                                 BlockPos pos,
-                                 IBlockState state,
-                                 EntityLivingBase placer,
-                                 ItemStack stack )
-    {
-        world.setBlockState( pos, state.withProperty( FACING, placer.getHorizontalFacing().getOpposite() ), 2 );
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer( this, FACING, STATE );
+    @Nonnull
+    protected net.minecraft.block.state.BlockStateContainer createBlockState() {
+        return new net.minecraft.block.state.BlockStateContainer( this, FACING, STATE );
     }
 
     @Override
@@ -220,14 +277,5 @@ public abstract class AbstractBrightLight extends Block
             shouldLight = true;
         }
         return shouldLight ? 15 : 0;
-    }
-
-    @Override
-    public boolean canConnectRedstone( IBlockState p_canConnectRedstone_1_,
-                                       IBlockAccess p_canConnectRedstone_2_,
-                                       BlockPos p_canConnectRedstone_3_,
-                                       @Nullable EnumFacing p_canConnectRedstone_4_ )
-    {
-        return true;
     }
 }
