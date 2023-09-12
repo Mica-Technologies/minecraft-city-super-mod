2a7c6beecebe431da2ecab33c7ea7aaef775b26b
mica-alex <83238954+mica-alex@users.noreply.github.com>
2023-09-12 14:20:35 -0400

    Stage 6.2 of major restructure

--- src/main/java/com/micatechnologies/minecraft/csm/lifesafety/AbstractBlockFireAlarmActivator.java
+++ src/main/java/com/micatechnologies/minecraft/csm/lifesafety/AbstractBlockFireAlarmActivator.java
@@ -1,5 +1,7 @@
 package com.micatechnologies.minecraft.csm.lifesafety;
 
+import com.micatechnologies.minecraft.csm.codeutils.AbstractBlockRotatableNSEWUD;
+import com.micatechnologies.minecraft.csm.codeutils.ICsmTileEntityProvider;
 import com.micatechnologies.minecraft.csm.tabs.CsmTabLifeSafety;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
@@ -21,74 +23,17 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public abstract class AbstractBlockFireAlarmActivator extends Block implements ITileEntityProvider
+public abstract class AbstractBlockFireAlarmActivator extends AbstractBlockRotatableNSEWUD
+        implements ICsmTileEntityProvider
 {
     public static final PropertyDirection FACING = BlockDirectional.FACING;
 
     public AbstractBlockFireAlarmActivator() {
-        super( Material.ROCK );
-        setUnlocalizedName( getBlockRegistryName() );
-        setSoundType( SoundType.STONE );
-        setHarvestLevel( "pickaxe", 1 );
-        setHardness( 2F );
-        setResistance( 10F );
-        setLightLevel( 0F );
-        setLightOpacity( 0 );
-        setCreativeTab( CsmTabLifeSafety.get() );
-        this.setDefaultState( this.blockState.getBaseState().withProperty( FACING, EnumFacing.NORTH ) );
-    }
-
-    abstract public String getBlockRegistryName();
-
-    @Override
-    public IBlockState getStateFromMeta( int meta ) {
-        return this.getDefaultState().withProperty( FACING, EnumFacing.getFront( meta ) );
-    }
-
-    @Override
-    public boolean canConnectRedstone( IBlockState p_canConnectRedstone_1_,
-                                       IBlockAccess p_canConnectRedstone_2_,
-                                       BlockPos p_canConnectRedstone_3_,
-                                       @Nullable EnumFacing p_canConnectRedstone_4_ )
-    {
-        return true;
-    }
-
-    @Override
-    public int getMetaFromState( IBlockState state ) {
-        return state.getValue( FACING ).getIndex();
-    }
-
-    @Override
-    public boolean isFullCube( IBlockState state ) {
-        return false;
-    }
-
-    @Override
-    public AxisAlignedBB getBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
-        switch ( state.getValue( FACING ) ) {
-            case SOUTH:
-            default:
-                return new AxisAlignedBB( 1D, 0D, 0.2D, 0D, 1D, 0D );
-            case NORTH:
-                return new AxisAlignedBB( 0D, 0D, 0.8D, 1D, 1D, 1D );
-            case WEST:
-                return new AxisAlignedBB( 0.8D, 0D, 1D, 1D, 1D, 0D );
-            case EAST:
-                return new AxisAlignedBB( 0.2D, 0D, 0D, 0D, 1D, 1D );
-            case UP:
-                return new AxisAlignedBB( 0D, 0.2D, 0D, 1D, 0D, 1D );
-            case DOWN:
-                return new AxisAlignedBB( 0D, 0.8D, 1D, 1D, 1D, 0D );
-        }
-    }
-
-    @Override
-    public boolean isOpaqueCube( IBlockState state ) {
-        return false;
+        super( Material.ROCK, SoundType.STONE, "pickaxe", 1, 2F, 10F, 0F, 0 );
     }
 
     @Override
@@ -116,30 +61,6 @@ public abstract class AbstractBlockFireAlarmActivator extends Block implements I
     public void onBlockAdded( World p_onBlockAdded_1_, BlockPos p_onBlockAdded_2_, IBlockState p_onBlockAdded_3_ ) {
         p_onBlockAdded_1_.scheduleUpdate( p_onBlockAdded_2_, this, this.tickRate( p_onBlockAdded_1_ ) );
         super.onBlockAdded( p_onBlockAdded_1_, p_onBlockAdded_2_, p_onBlockAdded_3_ );
-    }
-
-    @SideOnly( Side.CLIENT )
-    @Override
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.CUTOUT_MIPPED;
-    }
-
-    @Override
-    public IBlockState getStateForPlacement( World worldIn,
-                                             BlockPos pos,
-                                             EnumFacing facing,
-                                             float hitX,
-                                             float hitY,
-                                             float hitZ,
-                                             int meta,
-                                             EntityLivingBase placer )
-    {
-        return this.getDefaultState().withProperty( FACING, EnumFacing.getDirectionFromEntityLiving( pos, placer ) );
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer( this, FACING );
     }
 
     @Override
@@ -170,8 +91,93 @@ public abstract class AbstractBlockFireAlarmActivator extends Block implements I
         return activated;
     }
 
+    /**
+     * Retrieves the bounding box of the block.
+     *
+     * @param state  the block state
+     * @param source the block access
+     * @param pos    the block position
+     *
+     * @return The bounding box of the block.
+     *
+     * @since 1.0
+     */
     @Override
-    public TileEntity createNewTileEntity( World world, int i ) {
-        return new TileEntityFireAlarmSensor();
+    public AxisAlignedBB getBlockBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
+        return new AxisAlignedBB( 1D, 0D, 0.2D, 0D, 1D, 0D );
+    }
+
+    /**
+     * Retrieves whether the block is an opaque cube.
+     *
+     * @param state The block state.
+     *
+     * @return {@code true} if the block is an opaque cube, {@code false} otherwise.
+     *
+     * @since 1.0
+     */
+    @Override
+    public boolean getBlockIsOpaqueCube( IBlockState state ) {
+        return false;
+    }
+
+    /**
+     * Retrieves whether the block is a full cube.
+     *
+     * @param state The block state.
+     *
+     * @return {@code true} if the block is a full cube, {@code false} otherwise.
+     *
+     * @since 1.0
+     */
+    @Override
+    public boolean getBlockIsFullCube( IBlockState state ) {
+        return false;
+    }
+
+    /**
+     * Retrieves whether the block connects to redstone.
+     *
+     * @param state  the block state
+     * @param access the block access
+     * @param pos    the block position
+     * @param facing the block facing direction
+     *
+     * @return {@code true} if the block connects to redstone, {@code false} otherwise.
+     *
+     * @since 1.0
+     */
+    @Override
+    public boolean getBlockConnectsRedstone( IBlockState state,
+                                             IBlockAccess access,
+                                             BlockPos pos,
+                                             @Nullable EnumFacing facing )
+    {
+        return true;
+    }
+
+    /**
+     * Retrieves the block's render layer.
+     *
+     * @return The block's render layer.
+     *
+     * @since 1.0
+     */
+    @Nonnull
+    @Override
+    public BlockRenderLayer getBlockRenderLayer() {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+
+    /**
+     * Gets the tile entity class for the block.
+     *
+     * @return the tile entity class for the block
+     *
+     * @since 1.0
+     */
+    @Override
+    public Class< ? extends TileEntity > getTileEntityClass() {
+        return TileEntityFireAlarmSensor.class;
     }
 }
