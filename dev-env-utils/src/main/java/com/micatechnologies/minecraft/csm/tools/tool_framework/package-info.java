20408bdd981ed77c8c0696cb960429413816061c
mica-alex <83238954+mica-alex@users.noreply.github.com>
2023-10-19 19:48:23 -0400

    Add dev-env tool framework

---
+++ dev-env-utils/src/main/java/com/micatechnologies/minecraft/csm/tools/tool_framework/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package consists of the CSM development tools framework. This framework provides the foundation for the CSM
+ * development tools (dev-env-utils). These tools are used to assist in the development of CSM, and are not included in
+ * the final CSM mod JAR file.
+ *
+ * @version 1.0
+ * @since 2023.3
+ */
+package com.micatechnologies.minecraft.csm.tools.tool_framework;
