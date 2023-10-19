1c1179cb3143cccd65a1abb2360503598ff78e85
mica-alex <83238954+mica-alex@users.noreply.github.com>
2023-10-19 19:48:12 -0400

    Add dev-env tool package/info

---
+++ dev-env-utils/src/main/java/com/micatechnologies/minecraft/csm/tools/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package consists of the CSM development tools (dev-env-utils). These tools are used to assist in the development
+ * of CSM, and are not included in the final CSM mod JAR file.
+ *
+ * @version 1.0
+ * @since 2023.3
+ */
+package com.micatechnologies.minecraft.csm.tools;
