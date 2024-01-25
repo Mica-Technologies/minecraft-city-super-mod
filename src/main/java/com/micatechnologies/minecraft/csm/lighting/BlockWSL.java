dbcf7d865e3c809458d46e186d812b9a650bf393
mica-alex <83238954+mica-alex@users.noreply.github.com>
2024-01-25 17:17:22 -0500

    Fix some lighting bounding boxes

--- src/main/java/com/micatechnologies/minecraft/csm/lighting/BlockWSL.java
+++ src/main/java/com/micatechnologies/minecraft/csm/lighting/BlockWSL.java
@@ -24,8 +24,8 @@ public class BlockWSL extends AbstractBrightLight {
    * @since 1.0
    */
   @Override
-  public AxisAlignedBB getBlockBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-    return new AxisAlignedBB(0D, 0D, 0D, 1D, 0.5D, 1D);
+  public AxisAlignedBB getBlockBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
+    return new AxisAlignedBB(0.437500, 0.000000, 0.875000, 0.562500, 1.000000, 1.000000);
   }
 
   @Override
