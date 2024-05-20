e29adbc8816ad11e02cca2bf7f447cf64278b47d
Brandon Holt (RinRin Blissgreen) <rinrinblissgreen@gmail.com>
2024-05-20 14:42:50 -0700

    Update lang file and added more signs

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/BlockSeniorSafetyZoneSign.java
@@ -0,0 +1,9 @@
+package com.micatechnologies.minecraft.csm.trafficsigns;
+
+public class BlockSeniorSafetyZoneSign extends AbstractBlockSign {
+
+  @Override
+  public String getBlockRegistryName() {
+    return "seniorsafetyzonesign";
+  }
+}
