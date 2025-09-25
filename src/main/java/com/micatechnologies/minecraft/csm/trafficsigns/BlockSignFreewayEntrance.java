78bd407c37dce2698bb5817ea8a88df7b61d7a59
mica-alex <83238954+mica-alex@users.noreply.github.com>
2025-09-25 17:56:16 -0400

    More signs

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/BlockSignFreewayEntrance.java
@@ -0,0 +1,9 @@
+package com.micatechnologies.minecraft.csm.trafficsigns;
+
+public class BlockSignFreewayEntrance extends AbstractBlockSign {
+
+  @Override
+  public String getBlockRegistryName() {
+    return "signfreewayentrance";
+  }
+}
---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/BlockSignNoParkingBikeLane.java
@@ -0,0 +1,9 @@
+package com.micatechnologies.minecraft.csm.trafficsigns;
+
+public class BlockSignNoParkingBikeLane extends AbstractBlockSign {
+
+  @Override
+  public String getBlockRegistryName() {
+    return "signnoparkingbikelane";
+  }
+}
