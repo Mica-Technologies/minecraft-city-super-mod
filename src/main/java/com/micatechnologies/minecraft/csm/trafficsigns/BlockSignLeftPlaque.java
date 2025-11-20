a31f2dcf7f0583dd88c0fc0413c57ee78dbe796d
mica-alex <83238954+mica-alex@users.noreply.github.com>
2025-11-20 17:29:40 -0500

    Add some new signs

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/BlockSignLeftPlaque.java
@@ -0,0 +1,9 @@
+package com.micatechnologies.minecraft.csm.trafficsigns;
+
+public class BlockSignLeftPlaque extends AbstractBlockSign {
+
+  @Override
+  public String getBlockRegistryName() {
+    return "signleftplaque";
+  }
+}
---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsigns/BlockSignRightPlaque.java
@@ -0,0 +1,9 @@
+package com.micatechnologies.minecraft.csm.trafficsigns;
+
+public class BlockSignRightPlaque extends AbstractBlockSign {
+
+  @Override
+  public String getBlockRegistryName() {
+    return "signrightplaque";
+  }
+}
