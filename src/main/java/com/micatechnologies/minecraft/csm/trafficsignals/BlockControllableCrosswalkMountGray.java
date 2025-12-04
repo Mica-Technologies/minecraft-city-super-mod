69f7c9127b31364885a0a37d133bedfb46c819ce
mica-alex <83238954+mica-alex@users.noreply.github.com>
2025-12-04 13:57:28 -0500

    Add gray signals

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockControllableCrosswalkMountGray.java
@@ -0,0 +1,16 @@
+package com.micatechnologies.minecraft.csm.trafficsignals;
+
+public class BlockControllableCrosswalkMountGray extends AbstractBlockControllableCrosswalkSignal {
+
+  /**
+   * Retrieves the registry name of the block.
+   *
+   * @return The registry name of the block.
+   *
+   * @since 1.0
+   */
+  @Override
+  public String getBlockRegistryName() {
+    return "controllablecrosswalkmountgray";
+  }
+}
