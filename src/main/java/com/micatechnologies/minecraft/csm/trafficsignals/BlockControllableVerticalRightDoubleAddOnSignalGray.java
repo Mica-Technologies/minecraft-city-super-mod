9a41165a9bf437d3c9fbfbfb4e0d15ff2e3f5830
mica-alex <83238954+mica-alex@users.noreply.github.com>
2025-12-05 15:44:37 -0500

    Add up left and double add on gray signals

---
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficsignals/BlockControllableVerticalRightDoubleAddOnSignalGray.java
@@ -0,0 +1,36 @@
+package com.micatechnologies.minecraft.csm.trafficsignals;
+
+import com.micatechnologies.minecraft.csm.trafficsignals.logic.AbstractBlockControllableSignal;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockControllableVerticalRightDoubleAddOnSignalGray
+    extends AbstractBlockControllableSignal {
+
+  public BlockControllableVerticalRightDoubleAddOnSignalGray() {
+    super(Material.ROCK);
+  }
+
+  @Override
+  public SIGNAL_SIDE getSignalSide(World world, BlockPos blockPos) {
+    return SIGNAL_SIDE.RIGHT;
+  }
+
+  @Override
+  public boolean doesFlash() {
+    return false;
+  }
+
+  /**
+   * Retrieves the registry name of the block.
+   *
+   * @return The registry name of the block.
+   *
+   * @since 1.0
+   */
+  @Override
+  public String getBlockRegistryName() {
+    return "controllableverticalrightdoubleaddonsignalgray";
+  }
+}
