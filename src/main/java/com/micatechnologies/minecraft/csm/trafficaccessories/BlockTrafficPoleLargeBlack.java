489dd70a4c5e058fb41003860a232bdf9181a455
mica-alex <83238954+mica-alex@users.noreply.github.com>
2026-02-06 17:26:24 -0500

    Bbox improvements - large poles

--- src/main/java/com/micatechnologies/minecraft/csm/trafficaccessories/BlockTrafficPoleLargeBlack.java
+++ src/main/java/com/micatechnologies/minecraft/csm/trafficaccessories/BlockTrafficPoleLargeBlack.java
@@ -1,6 +1,10 @@
 package com.micatechnologies.minecraft.csm.trafficaccessories;
 
 import com.micatechnologies.minecraft.csm.codeutils.AbstractBlockTrafficPole;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockTrafficPoleLargeBlack extends AbstractBlockTrafficPole {
 
@@ -35,5 +39,21 @@ public class BlockTrafficPoleLargeBlack extends AbstractBlockTrafficPole {
   @Override
   public TRAFFIC_POLE_COLOR getTrafficPoleColor() {
     return TRAFFIC_POLE_COLOR.BLACK;
+  }
+
+  /**
+   * Retrieves the bounding box of the block.
+   *
+   * @param state  the block state
+   * @param source the block access
+   * @param pos    the block position
+   *
+   * @return The bounding box of the block.
+   *
+   * @since 1.0
+   */
+  @Override
+  public AxisAlignedBB getBlockBoundingBox( IBlockState state, IBlockAccess source, BlockPos pos ) {
+    return new AxisAlignedBB(0.125000, 0.125000, 0.000000, 0.875000, 0.875000, 1.000000);
   }
 }
